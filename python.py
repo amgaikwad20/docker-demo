db0f9732e9db08a64c77b5c2a288a95deb636327
Amol gaikwad <amgaikwad2025@gmail.com>
2026-07-01 01:56:50 -0700

    python commit

--- python.py
+++ python.py
@@ -18,3 +18,26 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+    #!/usr/bin/env python3
+
+def write_file(filename):
+    with open(filename, "w") as file:
+        file.write("Welcome to DevOps!\n")
+        file.write("Python is useful for automation.\n")
+    print(f"Data written to {filename}")
+
+def read_file(filename):
+    with open(filename, "r") as file:
+        print("\nFile Contents:")
+        print(file.read())
+
+def main():
+    filename = "devops.txt"
+
+    write_file(filename)
+    read_file(filename)
+
+if __name__ == "__main__":
+    main()
